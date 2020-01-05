f4a1272d23f2c7fe02bbebd3ec577df8a6b171b1
beltramem <beltramo.emeric@gmail.com>
2020-01-05 11:33:17 +0100

    modif avant boucle

--- SiloMaster2K20/src/main/java/Engine/Clock.java
+++ SiloMaster2K20/src/main/java/Engine/Clock.java
@@ -8,7 +8,10 @@ package Engine;
 import Model.Boisseau;
 import Model.Cellule;
 import Model.FosseReception;
+import Model.PontBascule;
 import Model.Tremie;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -17,39 +20,43 @@ import Model.Tremie;
  */
 public class Clock implements Runnable {
     public void run()
-    {
-        GestionEvenement ge = new GestionEvenement();
+    {   
+        List<Cellule> cellules = new ArrayList<Cellule>();
         
-        Cellule c1 = (Cellule) ge.creerPoste("Cellule", 1);
-        Cellule c2 = (Cellule) ge.creerPoste("Cellule", 2);
-        Cellule c3 = (Cellule) ge.creerPoste("Cellule", 3);
-        Cellule c4 = (Cellule) ge.creerPoste("Cellule", 4);
-        Cellule c5 = (Cellule) ge.creerPoste("Cellule", 5);
-        Cellule c6 = (Cellule) ge.creerPoste("Cellule", 6);
-        Cellule c7 = (Cellule) ge.creerPoste("Cellule", 7);
-        Cellule c8 = (Cellule) ge.creerPoste("Cellule", 8);
-        Cellule c9 = (Cellule) ge.creerPoste("Cellule", 9);
-        Cellule c10 = (Cellule) ge.creerPoste("Cellule", 10);
         
-        Boisseau bo1 = (Boisseau) ge.creerPoste("Boisseau", 1);
-        Boisseau bo2 = (Boisseau) ge.creerPoste("Boisseau", 2);
-        Boisseau bo3 = (Boisseau) ge.creerPoste("Boisseau", 3);
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 1));
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 2));
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 3));
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 4));
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 5));
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 6));
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 7));
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 8));
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 9));
+        cellules.add((Cellule) GestionEvenement.getInstance().creerPoste("Cellule", 10));
         
-        FosseReception fr1 = (FosseReception) ge.creerPoste("FosseReception", 1);
-        FosseReception fr2 = (FosseReception) ge.creerPoste("FosseReception", 2);
+        List<Boisseau> Boisseaux = new ArrayList<Boisseau>();
         
-        Tremie tr1 = (Tremie) ge.creerPoste("Tremie", 1);
-        Tremie tr2= (Tremie) ge.creerPoste("Tremie", 2);
-        /*
+        Boisseaux.add((Boisseau) GestionEvenement.getInstance().creerPoste("Boisseau", 1));
+        Boisseaux.add((Boisseau) GestionEvenement.getInstance().creerPoste("Boisseau", 2));
+        Boisseaux.add((Boisseau) GestionEvenement.getInstance().creerPoste("Boisseau", 3));
         
-        Tremie tr1 = new Tremie(1);
-        Tremie tr2 = new Tremie(2);
+        List<FosseReception> FosseReceptions = new ArrayList<FosseReception>();
         
-        PontBascule pb1 = new PontBascule(1);*/
+        FosseReceptions.add((FosseReception) GestionEvenement.getInstance().creerPoste("FosseReception", 1));
+        FosseReceptions.add((FosseReception) GestionEvenement.getInstance().creerPoste("FosseReception", 2));
+        
+        List<Tremie> tremies = new ArrayList<Tremie>();
+        
+        tremies.add((Tremie) GestionEvenement.getInstance().creerPoste("Tremie", 1));
+        tremies.add((Tremie) GestionEvenement.getInstance().creerPoste("Tremie", 2));
+        
+        PontBascule pb1 = (PontBascule) GestionEvenement.getInstance().creerPoste("PontBascule", 1);
         
         while (true)
         {
-            
+            int chanceNouveauLot = (int) Math.ceil(Math.random() * 10);
+            if(chanc)
         }
     }
 }
